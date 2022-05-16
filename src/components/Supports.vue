559cebb0c0fa9e55522a8abab568fae0ac1ff942
fdesiqueira.ti <fdesiqueira.ti@gmail.com>
2022-05-16 16:58:46 -0300

    Ocultar ou exibir respostas do suporte.

--- src/components/Supports.vue
+++ src/components/Supports.vue
@@ -1,7 +1,5 @@
-<template>
-     
+<template>     
     <div class="comments">
-
         <div class="content">
 
             <div class="card" v-for="support in supports.data" :key="support.id">
@@ -9,7 +7,7 @@
                     <span class="avatar">
                         <img :src="[ support.user.image ? 
                                      support.user.image :
-                                     '@/assets/images/avatars/user01.svg']" 
+                                     require('@/assets/images/avatars/user01.svg')]" 
                              :alt="support.user.name" />
                     </span>
                     <div class="comment">
@@ -21,30 +19,46 @@
                             </span>
                         </div>
                     </div>
-                    <button class="btn primary">Ocultar respostas</button>
+                    <button class="btn primary">
+                        <span v-if="showSupport === support.id" @click.prevent="showSupport = '0'">Ocultar respostas</span>  
+                        <span v-else                            @click.prevent="showSupport = support.id">Exibir respostas</span>                        
+                    </button>
                 </div>
-                <div class="answersContent" v-for="reply in support.replies" :key="reply.id">
-                    <div class="commentContent rightContent">
+                <div class="answersContent" v-show="showSupport === support.id">
+                    <div :class="[
+                            'commentContent',
+                            { 'rightContent' : support.user.id != reply.user.id } 
+                        ]"  
+                        v-for="reply in support.replies" 
+                        :key="reply.id">
+                         <span class="avatar" v-if="support.user.id == reply.user.id">
+                            <img :src="[ reply.user.image ? 
+                                         reply.user.image : 
+                                         require('@/assets/images/avatars/user01.svg')]" 
+                                 :alt="reply.user.name" />
+                        </span>
                         <div class="comment">
                             <div class="balloon">
                                 <span class="fas fa-sort-down"></span>
-                                <span class="owner">Carlos Ferreira - 07/10/2021</span>
+                                <span class="owner">{{ reply.user.name }}} - {{ reply.dt_uptdated }}</span>
                                 <span class="text">
                                     {{ reply.description }}
                                 </span>
                             </div>
                         </div>
-                        <span class="avatar">
-                            <img src="images/avatars/user03.svg" alt="">
-                           <!--  <img :src="[ reply.user.image ? 
+                        <span class="avatar" v-if="support.user.id != reply.user.id">
+                            <img :src="[ reply.user.image ? 
                                          reply.user.image : 
-                                         '@/assets/images/avatars/user03.svg']" 
-                                 :alt="reply.user.name" /> -->
+                                         require('@/assets/images/avatars/user03.svg')]" 
+                                 :alt="reply.user.name" />
                         </span>
                     </div>
                     <div class="commentContent">
                         <span class="avatar">
-                            <img src="images/avatars/user01.svg" alt="">
+                             <img :src="[ support.user.image ? 
+                                     support.user.image :
+                                     require('@/assets/images/avatars/user01.svg')]" 
+                             :alt="support.user.name" />
                         </span>
                         <div class="comment">
                             <div class="balloon">
@@ -63,13 +77,11 @@
             </div>
         </div>
     </div>
-
-
 </template>
 
 <script>
     import { useStore } from 'vuex'
-    import { computed } from 'vue'
+    import { computed, ref } from 'vue'
 
     export default {
         // eslint-disable-next-line vue/multi-word-component-names
@@ -78,11 +90,13 @@
             const store = useStore()
 
             const supports = computed(() => store.state.supports.supports )
+
+            const showSupport = ref('0')
          
             return {
                 supports,
+                showSupport
             }
         }       
     }
-
 </script>
